fc4bba487976e2214bafd8a51fd9a47e4b0987f0
Senih <vigilant@Senihs-MacBook-Air.local>
2024-03-03 16:56:06 -0500

    Setting up partial parts & Page files

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ const path = require('path');
 
 let app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
 
+// Routes
 app.get('/', function(req, res){
   res.render('home');
 });
 
-app.listen(3000, function() {
-  console.log('TechTravel running successfully on port 3000!');
+app.get('/search', function(req, res) {
+  res.render('search');
+});
+
+app.get('/itinerary', function(req, res) {
+  res.render('itinerary');
+});
+
+app.get('/tracker', function(req, res) {
+  res.render('tracker');
+});
+
+app.get('/profile', function(req, res) {
+  res.render('profile');
 })
+
+app.listen(3000, function() {
+  console.log('App is running on http://localhost:3000/');
+});
