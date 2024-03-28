3a9b79fbf24b1c53c7520fda37b6260031fbaf0e
Mhoney2802 <mhoneycutt2002@gmail.com>
2024-03-28 17:04:17 -0400

    Itinerary Edit Route Revision

--- routes/index.js
+++ routes/index.js
@@ -204,23 +204,18 @@ router.get('/itinerary/edit/:id', async (req, res) => {
     const itinerary = await Itinerary.findById(itineraryId);
     res.render('itinerary-edit', { itinerary });
   }   catch (error) {
-    res.status(500).json({ message: 'Failed to fetch itinerary', error: error.message });
+    res.status(500).json({ message: 'Failed to fetch itinerary for edit', error: error.message });
   }
 })
 
-router.get('/itinerary/edit/:id', async (req, res) => {
+router.post('/itinerary/edit/:id', async (req, res) => {
   try {
     // Retrieve the itinerary ID from the request parameters
     const itineraryId = req.params.id;
 
     // Find the itinerary in the database by its ID
     const itinerary = await Itinerary.findById(itineraryId);
-
-    // Extract user information
-    const author = {
-      id: req.user._id,
-      username: req.user.username
-    };
+    console.log(itinerary)
 
     // Extract data from req.body
     const { itineraryName, startingCity, startDate, endDate, destinations } = req.body;
@@ -242,22 +237,19 @@ router.get('/itinerary/edit/:id', async (req, res) => {
       }));
     }
 
-    // Create a new itinerary object
-    itinerary = new Itinerary({
-      author, // Include the author information
-      itineraryName,
-      startingCity,
-      startDate,
-      endDate,
-      destinations: formattedDestinations // Use the formatted destinations
-    });
+    // Update the itinerary object with new values
+    itinerary.itineraryName = itineraryName;
+    itinerary.startingCity = startingCity;
+    itinerary.startDate = startDate;
+    itinerary.endDate = endDate;
+    itinerary.destinations = formattedDestinations;
 
-    // Save the new itinerary to the database
+    // Save the updated itinerary to the database
     await itinerary.updateOne();
 
     res.status(201).redirect('/itinerary/view/:id');
   } catch (error) {
-    res.status(500).json({ message: 'Failed to create itinerary', error: error.message });
+    res.status(500).json({ message: 'Failed to update itinerary', error: error.message });
   }
 });
 
